939cd74d9556a6b1fbdffae73f14a6cbc9b2ca2d
JaxH2003 <hefkejac@msu.edu>
2024-03-25 17:05:51 +0000

    words changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          What a ool way to develop an app
+          What a cool way to develop an app
         </p>
         <a
           className="App-link"
