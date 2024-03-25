fb1e1947bac1fbdf5a5baa483aa7d1fedb9a4f4b
JaxH2003 <hefkejac@msu.edu>
2024-03-25 17:04:54 +0000

    words changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What a ool way to develop an app
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I Love React!
         </a>
       </header>
     </div>
