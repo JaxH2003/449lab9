8e98c57b4168e079a584d277e05f4e860bb91537
JaxH2003 <hefkejac@msu.edu>
2024-03-31 22:10:26 +0000

    adding a movie showcase+ components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+function EpicButton() {
+  return (
+    <div>
+      <h3>This Button is Pretty Epic!</h3>
+      <button>Epic</button>
+      </div>
+  );
+}
+
+const movie = {
+  title: 'Star Wars a New Hope',
+  director: 'George Lucas',
+  release: '1977',
+  image: logo,
+  width: '250',
+  height: '250'
+};
+
+function MovieCase() {
+  return (
+    <>
+      <h2>{movie.title} ({movie.release})</h2>
+      <p>{movie.director}</p>
+      <img
+        className="movieCover"
+        src={movie.image}
+        alt={movie.title + ' cover'}
+        style={{
+          width: movie.width,
+          height: movie.height
+        }}
+      />
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          What a cool way to develop an app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I Love React!
-        </a>
+        <MovieCase />
+        <EpicButton />
       </header>
     </div>
   );
